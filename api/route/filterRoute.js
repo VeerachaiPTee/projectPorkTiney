81fe05798c0deceb20731c808aec11e06e252faf
VeerachaiPTee <devtee.pp@gmail.com>
2024-05-05 00:22:36 +0700

    first commit

---
+++ api/route/filterRoute.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const router = express.Router()
+const uploadImg = require('../middleware/upload')
+const {
+  handleDeleteData,
+  handleUpdateProduct,
+} = require('../controller/productController')
+const {
+  handleAllProduct,
+  handleMinusTotal,
+  handlePlusTotal,
+  handleCheckSlip,
+  handleUserPayment,
+  handleAllAdmin,
+  handleDeleteAdmin,
+  handleaddAdmin,
+  handleselectBillOrder,
+  handleUpdateStatusBill,
+  handleSelectAllAmountSell,
+  handlesumPriceB,
+  handlesumForChart,
+  handleselectBillConfirm,
+  generateBilling
+} = require('../controller/filterController')
+
+router.get('/listProduct', handleAllProduct)
+router.post('/deleteProduct', handleDeleteData)
+router.post('/updateProduct', handleUpdateProduct)
+router.post('/minusProduct', handleMinusTotal)
+router.post('/plusProduct', handlePlusTotal)
+router.post('/slipCheck', uploadImg.single('files'), handleCheckSlip)
+router.post('/confirmPayment', handleUserPayment)
+router.get('/listAdmin', handleAllAdmin)
+router.post('/deleteAdmin', handleDeleteAdmin)
+router.post('/addAdmin', handleaddAdmin)
+router.get('/allBill', handleselectBillOrder)
+router.post('/udBill', handleUpdateStatusBill)
+router.get('/allAmountSell', handleSelectAllAmountSell)
+router.get('/sumpriceb', handlesumPriceB)
+router.get('/sumForChart', handlesumForChart)
+router.get('/selectBillConfirm', handleselectBillConfirm)
+router.post('/getDataPrint', generateBilling)
+
+module.exports = router
